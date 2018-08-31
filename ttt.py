f132ec197afb43f43ec02f9585ec97adffdb7d8d
Thomas Philip Runarsson <tprunarsson@users.noreply.github.com>
2018-08-31 06:57:50 +0000

    Update ttt.py
    added comments and update for draws (pretty print debug)

--- ttt.py
+++ ttt.py
@@ -38,37 +38,42 @@ def getotherplayer(player):
 def learnit(numgames, epsilon, alpha, debug=False):
     # play games for training
     for games in range(0, numgames):
-        state = np.zeros(9)        # initialize the board
-        s = [hashit(state), hashit(state)] # this state value is never used
-        # player to start is "+1"
+        board = np.zeros(9)          # initialize the board
+        sold = [0, hashit(board), 0] # first element not used
+        # player to start is "1" the other player is "2"
         player = 1
         # start turn playing game, maximum 9 moves
         for move in range(0, 9):
-            # use policy to find action
-            action = epsilongreedy(np.copy(state), player, epsilon)
-            # perform action and observe new state (for other player)
-            state[action] = player
-            # print the board, if in debug mode
+            # use a policy to find action
+            action = epsilongreedy(np.copy(board), player, epsilon)
+            # perform move and update board (for other player)
+            board[action] = player
+            # print the board, when in debug mode
             if debug:
-                print(state.reshape(3, 3))
+                symbols = np.array([" ", "X", "O"])
+                print("player ", symbols[player], ", move number ", move+1, ":")
+                print(symbols[board.astype(int)].reshape(3,3))
             # has this player won?
-            if (1 == iswin(state, player)):
-                value[s[player - 1]] = value[s[player - 1]] + alpha * (1.0 - value[s[player - 1]])
-                s[player - 1] = hashit(state) # index to winning state
-                value[s[player - 1]] = 1.0 # winner (reward one)
-                value[s[getotherplayer(player) - 1]] = 0.0 # looser, reward zero
-                # also do a temporal update for the previous state
+            if (1 == iswin(board, player)):
+                value[sold[player]] = value[sold[player]] + alpha * (1.0 - value[sold[player]])
+                sold[player] = hashit(board) # index to winning state
+                value[sold[player]] = 1.0 # winner (reward one)
+                value[sold[getotherplayer(player)]] = 0.0 # looser (reward zero)
                 break
-            # do a temporal difference update
-            value[s[player - 1]] = value[s[player - 1]] + alpha * (value[hashit(state)] - value[s[player - 1]])
-            s[player - 1] = hashit(state) # store this new state for player
+            # do a temporal difference update, once both players have made at least one move
+            if (move > 1):
+                value[sold[player]] = value[sold[player]] + alpha * (value[hashit(board)] - value[sold[player]])
+            sold[player] = hashit(board) # store this new state for player
+            # check if we have a draw, then set the final states for both players to 0.5
+            if (move == 8):
+                value[sold] = 0.5 # draw (equal reward for both)
             player = getotherplayer(player) # swap players
 
-# do trials here, note because we initialize everything to 0.5 we don't need
-# to update for draws 
+# global after-state value function, note this table is too big and contrains states that
+# will never be used, also each state is unique to the player (no one after-state seen by both players) 
 value = np.ones(hashit(2 * np.ones(9))) / 2.0
-alpha = 0.1
-epsilon = 0.1
+alpha = 0.1 # step size
+epsilon = 0.1 # exploration parameter
 # train the value function
 learnit(10000, epsilon, alpha)
 # play one game determinstically using the value function
